bd7791f8a1b051ca7e0ac3bb9f993c016d74daf8
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-04-24 19:02:18 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -87,6 +87,7 @@ const User = mongoose.model('users',userSchema);
 // for backend and express
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 app.use(express.json());
 
