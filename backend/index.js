957e75853d7f94105f2b67f14afad21bf0794844
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-04-25 00:06:13 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://localhost:27017/', {
 //Defining Schema for users of the app
 const userSchema = new Schema({
     _id:{
-        type: mongoose.Schema.Types.ObjectId,
+        type: String,
         required: true,
     },
     accountName:{
