d667913019fadda71bd90ddbe2824f3e7ee25064
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-04-25 17:22:29 +0530

    changes

---
+++ backend/models/user.js
@@ -0,0 +1,84 @@
+//to connect with the mongoDB database
+const mongoose = require('mongoose');
+const Schema =  mongoose.Schema;
+
+mongoose.connect('mongodb://localhost:27017/', {
+    dbName: 'payByText',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+        }, err => err ? console.log(err) : 
+            console.log('Connected to payByText database'));
+
+
+//Defining Schema for users of the app
+const userSchema = new Schema({
+    accountName:{
+        type: String,
+        required: true
+    },
+
+    active:{
+        type: Boolean,
+        required: true,
+    },
+
+    accountNumber:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    createdOn:{
+        type: Date,
+        required: true,
+    },
+
+    issuer:{
+        type: String,
+        required: true,
+    },
+
+    paymentType:{
+        type: String,
+        required: true,
+    },
+
+    modifiedOn:{
+        type: Date,
+        required: false,
+    },
+
+    merchantId:{
+        type: String,
+        required: true,
+    },
+
+    createdBy:{
+        type: String,
+        required: true,
+    },
+
+    executeFlag:{
+        type: String,
+        required: true,
+    },
+
+    identityId:{
+        type: String,
+        required: true,
+    },
+
+    paymentMethod:{
+        walletId:{
+            type: String,
+            required: false,
+        }
+    },
+
+    modifiedBy:{
+        type: String,
+        required: true,
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User',userSchema);
