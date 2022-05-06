ed012fe343f0e2ccc0bb97b79a76fdf75d5efa49
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-06 20:17:12 +0530

    doing it

--- src/Preferences.js
+++ src/Preferences.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Preferences = () => {
+    const [name, setName]= useState("");
+    const [userID, setUserID] = useState("");
   return (
     <div>
         <p>My Dashboard {">"} My Preferences</p>
         <h2>My Preferences</h2>
         <i>* Indicates Required Field</i>
         <br/>
+        <div>
+            <form>
+                <label>
+                    Name
+                    <input type="text" name="name" value={name} 
+                    onChange={(e)=>{
+                        setName(e.target.value);
+                        console.log(name);
+                    }} />
+                </label>
+
+                <label>
+                    User ID
+                    <input type="text" name='' />
+                </label>
+            </form>
+        </div>
     </div>
   )
 }
