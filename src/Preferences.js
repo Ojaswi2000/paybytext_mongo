d9e778fdb1e9412067fe214c6dec3e98a447a8fd
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-06 19:02:59 +0530

    started preferences.js

---
+++ src/Preferences.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Preferences = () => {
+  return (
+    <div>
+        <h4>My Dashboard `{'>'}` My Preferences</h4>
+    </div>
+  )
+}
+
+export default Preferences
