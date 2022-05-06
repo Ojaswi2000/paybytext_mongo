3f4806d91ba3400d97083414d0369fbebafea161
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-07 00:33:49 +0530

    form

--- src/Preferences.js
+++ src/Preferences.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import {Form,Button, Textarea, ButtonToolbar} from  'rsuite';
+import 'rsuite/dist/rsuite.min.css';
+
 import './styles.css'
 
 const Preferences = () => {
@@ -11,7 +14,28 @@ const Preferences = () => {
         <i>* Indicates Required Field</i>
         <br/>
         <div className='preference'>
-        
+        <Form layout="horizontal">
+            <Form.Group controlId="name-6">
+              <Form.ControlLabel>Username</Form.ControlLabel>
+              <Form.Control name="name" />
+              <Form.HelpText>Required</Form.HelpText>
+            </Form.Group>
+            <Form.Group controlId="email-6">
+              <Form.ControlLabel>Email</Form.ControlLabel>
+              <Form.Control name="email" type="email" />
+              <Form.HelpText tooltip>Required</Form.HelpText>
+            </Form.Group>
+            <Form.Group controlId="password-6">
+              <Form.ControlLabel>Password</Form.ControlLabel>
+              <Form.Control name="password" type="password" autoComplete="off" />
+            </Form.Group>
+            <Form.Group>
+              <ButtonToolbar>
+                <Button appearance="primary">Submit</Button>
+                <Button appearance="default">Cancel</Button>
+              </ButtonToolbar>
+            </Form.Group>
+        </Form>
   
         </div>
     </div>
