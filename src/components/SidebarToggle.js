0880294a2bee8d5dba9ae388bbb1aba0bc810b19
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-08 23:52:46 +0530

    changes

--- src/components/SidebarToggle.js
+++ src/components/SidebarToggle.js
@@ -1,3 +1,5 @@
+import React,{useState} from 'react'
+
 const styles = {
   radioGroupLabel: {
     padding: '8px 12px',
@@ -5,3 +7,19 @@ const styles = {
     verticalAlign: 'middle'
   }
 };
+
+ const SidebarToggle = () => {
+  const [size, setSize] = useState('xs');
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState();
+
+  const handleOpen = key => {
+    setOpen(true);
+    setPlacement(key);
+  };
+  return (
+    <div></div>
+  )
+}
+
+export default SidebarToggle
