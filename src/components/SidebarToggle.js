c5e54d3306ed016a0b90be51e1e99116ee73c24d
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-08 21:33:41 +0530

    navToggle

--- src/components/SidebarToggle.js
+++ src/components/SidebarToggle.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import {Nav, Navbar, Dropdown, Sidebar, Sidenav} from 'rsuite'
+import {Nav, Navbar, Dropdown, Sidebar, Sidenav, AngleLeft, AngleRight} from 'rsuite'
 
 const headerStyles = {
     padding: 18,
@@ -51,8 +51,67 @@ const headerStyles = {
 const SidebarToggle = () => {
   const [expand, setExpand] = useState(true);
   return (
-    <div>
+    <div className="show-fake-browser sidebar-page">
+      <Container>
+        <Sidebar
+          style={{ display: 'flex', flexDirection: 'column' }}
+          width={expand ? 260 : 56}
+          collapsible
+        >
+          <Sidenav.Header>
+            <div style={headerStyles}>
+              <LogoAnalytics style={{ fontSize: 20 }} />
+              <span style={{ marginLeft: 12 }}> BRAND</span>
+            </div>
+          </Sidenav.Header>
+          <Sidenav expanded={expand} defaultOpenKeys={['3']} appearance="subtle">
+            <Sidenav.Body>
+              <Nav>
+                <Nav.Item eventKey="1" active icon={<Dashboard />}>
+                  Dashboard
+                </Nav.Item>
+                <Nav.Item eventKey="2" icon={<Group />}>
+                  User Group
+                </Nav.Item>
+                <Dropdown
+                  eventKey="3"
+                  trigger="hover"
+                  title="Advanced"
+                  icon={<Magic />}
+                  placement="rightStart"
+                >
+                  <Dropdown.Item eventKey="3-1">Geo</Dropdown.Item>
+                  <Dropdown.Item eventKey="3-2">Devices</Dropdown.Item>
+                  <Dropdown.Item eventKey="3-3">Brand</Dropdown.Item>
+                  <Dropdown.Item eventKey="3-4">Loyalty</Dropdown.Item>
+                  <Dropdown.Item eventKey="3-5">Visit Depth</Dropdown.Item>
+                </Dropdown>
+                <Dropdown
+                  eventKey="4"
+                  trigger="hover"
+                  title="Settings"
+                  icon={<GearCircle />}
+                  placement="rightStart"
+                >
+                  <Dropdown.Item eventKey="4-1">Applications</Dropdown.Item>
+                  <Dropdown.Item eventKey="4-2">Websites</Dropdown.Item>
+                  <Dropdown.Item eventKey="4-3">Channels</Dropdown.Item>
+                  <Dropdown.Item eventKey="4-4">Tags</Dropdown.Item>
+                  <Dropdown.Item eventKey="4-5">Versions</Dropdown.Item>
+                </Dropdown>
+              </Nav>
+            </Sidenav.Body>
+          </Sidenav>
+          <NavToggle expand={expand} onChange={() => setExpand(!expand)} />
+        </Sidebar>
 
+        <Container>
+          <Header>
+            <h2>Page Title</h2>
+          </Header>
+          <Content>Content</Content>
+        </Container>
+      </Container>
     </div>
   )
 }
