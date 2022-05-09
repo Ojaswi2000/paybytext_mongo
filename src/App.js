6aefda3929118ed481215b8bb28931850feb718b
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-09 23:11:35 +0530

    changes in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Preferences from './Preferences'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import MyAccountInvoices from './components/pages/MyAccountInvoices'
+import MyAutomaticPayments from './components/pages/MyAutomaticPayments'
+import Wallet from './components/pages/Wallet'
+import MyPlanInvoices from './components/pages/MyPlanInvoices'
 import './styles.css'
 
 const App = () => {
   return (
     <div className='App'>
-      <Preferences />
+      <Router>
+        <Routes>
+          <Route path='/preferences' element={<Preferences />} />
+          <Route path='/MyAutomaticPayments' element={<MyAutomaticPayments />} />
+          <Route path='/MyPlanInvoices' element={<MyPlanInvoices />} />
+          <Route path='/Wallet' element={<Wallet />} />
+          <Route path='/MyAccountInvoices' element={<MyAccountInvoices />} />
+        </Routes>
+      </Router>
+      
     </div>
   )
 }
