fd42847c3257a5f77a75b4fe371ceb12b0aa4218
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-10 00:40:00 +0530

    top section styled

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {motion} from 'framer-motion'
-import {FaHome} from 'react-icons/fa'
+import {FaHome,FaUser,FaBars} from 'react-icons/fa'
+import {MdMessage} from 'react-icons/md'
+import {BiAnalyse,BiSearch} from 'react-icons/bi'
 import '../styles.css';
 import { NavLink } from 'react-router-dom';
 
@@ -9,13 +11,43 @@ const routes = [
     path:"/",
     name:"Preferences",
     icon:<FaHome />
-  }
+  },
+  {
+    path: "/MyAutomaticPayments",
+    name: "MyAutomaticPayments",
+    icon: <FaUser />,
+  },
+  {
+    path: "/MyPlanInvoices",
+    name: "MyPlanInvoices",
+    icon: <MdMessage />,
+  },
+  {
+    path: "/Wallet",
+    name: "Wallet",
+    icon: <BiAnalyse />,
+  },
 ]
 
 const Sidebar = ({children}) => {
   return (
     <div className='main-container' >
         <motion.div animate={{width:"200px"}} className='sidebar' >
+
+          <div className='top_section'>
+            <h1 className='logo'>UPS</h1>
+            <div className='bars'>
+              <FaBars />
+            </div>
+          </div>
+
+
+          <div className='search'>
+            <div className='search_icon'>
+              <BiSearch />
+            </div>
+            <input placeholder='Search...' />
+          </div>
 
           <section className='routes' >
             {routes.map((route) => {
