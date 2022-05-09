d09768a4237ab3397936fdd3761839d105f81281
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-09 23:44:34 +0530

    sidebar in construction

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import {motion} from 'framer-motion'
+import {FaHome} from 'react-icons/fa'
 import '../styles.css';
+import { NavLink } from 'react-router-dom';
+
+const routes = [
+  {
+    path:"/",
+    name:"Preferences",
+    icon:<FaHome />
+  }
+]
 
 const Sidebar = ({children}) => {
   return (
     <div className='main-container' >
-        <motion.div animate={{width:"200px"}} className='sidebar' ></motion.div>
+        <motion.div animate={{width:"200px"}} className='sidebar' >
+
+          <section className='routes' >
+            {routes.map((route) => {
+              return <NavLink to={route.path} key={route.name}>
+                    <div className='icon'>
+                      {route.icon}
+                    </div>
+                </NavLink>
+            })}
+          </section>
+
+        </motion.div>
         <main>
           {children}
         </main>
