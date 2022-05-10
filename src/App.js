d93d9e3626d144fc31fa73627eb8938e8f921bb7
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-11 00:51:51 +0530

    introduced paybytext

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MyAccountInvoices from './components/pages/MyAccountInvoices'
 import MyAutomaticPayments from './components/pages/MyAutomaticPayments'
 import Wallet from './components/pages/Wallet'
 import MyPlanInvoices from './components/pages/MyPlanInvoices'
+import Paybytext from './Paybytext'
 import './styles.css'
 import Sidebar from './components/Sidebar'
 
@@ -20,6 +21,7 @@ const App = () => {
             <Route path='/MyPlanInvoices' element={<MyPlanInvoices />} />
             <Route path='/Wallet' element={<Wallet />} />
             <Route path='/MyAccountInvoices' element={<MyAccountInvoices />} />
+            <Route path='/PayByText' element={<Paybytext />} />
           </Routes>
         </Sidebar>
       </Router>
---
+++ src/Paybytext.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Paybytext = () => {
+  return (
+    <div>Paybytext</div>
+  )
+}
+
+export default Paybytext
