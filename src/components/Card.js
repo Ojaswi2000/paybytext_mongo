5bfd65911b16a69fde0dc34bf21127fe707db31f
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-12 23:58:16 +0530

    done with the radio

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles.css'
 import {Panel} from 'rsuite'
 
 const Card = ({account}) => {
+  const [check,setCheck] = useState(false);
+  const [acc, setAcc] = useState(null);
+
+  console.log(acc);
   return (
     <Panel className='card' shaded>
       <div>
-        <input style={{margin:"0",padding:"1.5px"}} type="radio" />
+        <input style={{margin:"0",padding:"1.5px"}} 
+          type="radio" 
+          name='account'
+          value={account}
+          checked={acc === account}
+          onChange={() => setAcc(account)}
+        />
       </div>
       <div>
           <p style={{marginBottom:"2px",fontWeight:"bold"}}>{account.accountName}</p>
