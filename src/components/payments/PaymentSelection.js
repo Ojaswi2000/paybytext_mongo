eecd5b5acb662ba085ebe7d0b90b190894856b47
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-17 01:43:55 +0530

    changed

--- src/components/payments/PaymentSelection.js
+++ src/components/payments/PaymentSelection.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import {Modal} from 'rsuite'
+import '../../styles.css'
 
 const PaymentSelection = ({step,setStep}) => {
   return (
     <div>
       <h3>Payment Method</h3>
+      <table className='tab'>
+        <thead>
+          <tr>
+            <th>Account Name</th>
+            <th>Account Number</th>
+            <th>Bank Direct Deposit Only</th>
+          </tr>
+        </thead>
+      </table>
     </div>
     
   )
