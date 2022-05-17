fd6226d10d9d611a9a38102ae1e0a9a12af35e78
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-17 16:53:19 +0530

    border

--- src/components/payments/Confirmation.js
+++ src/components/payments/Confirmation.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import '../../styles.css'
 
+
 const Confirmation = ({selectedCardId}) => {
   return (
     <div style={{marginTop:"30px"}}>
         <h4 style={{borderBotton:"2px solid cyan"}}>Confirmation Screen</h4>
-        <table className='tab'>
-            <tr>
-                <td></td>
-            </tr>
-        </table>
+        <div className='confirm'>
+
+        </div>
     </div>
   )
 }
