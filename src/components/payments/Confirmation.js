8be29fade955248dd9fa3c11849a6081cbfbd222
Ojaswi2000 <ojaswiawasthi2025@gmail.com>
2022-05-17 17:04:29 +0530

    done with confirmation

--- src/components/payments/Confirmation.js
+++ src/components/payments/Confirmation.js
@@ -7,7 +7,26 @@ const Confirmation = ({selectedCardId}) => {
     <div style={{marginTop:"30px"}}>
         <h4 style={{borderBotton:"2px solid cyan"}}>Confirmation Screen</h4>
         <div className='confirm'>
-
+            <div style={{borderBottom: "1px solid grey"}}>
+                <h6>Confirmation Number:</h6>
+                <p>{selectedCardId._id}</p>
+            </div>
+            <div style={{borderBottom: "1px solid grey"}}>
+                <h6>Account Number:</h6>
+                <p>{selectedCardId.accountNumber}</p>
+            </div>
+            <div style={{borderBottom: "1px solid grey"}}>
+                <h6>Account Name:</h6>
+                <p>{selectedCardId.accountName}</p>
+            </div>
+            <div style={{borderBottom: "1px solid grey"}}>
+                <h6>Payment Method:</h6>
+                {
+                    selectedCardId.paymentMethod.walletId === undefined ?
+                    <p>This account has no wallet ID</p>:
+                    <p>{selectedCardId.paymentMethod.walletId}</p>
+                }
+            </div>
         </div>
     </div>
   )
